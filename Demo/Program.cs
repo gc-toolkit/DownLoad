c136525b300c4ff858e74dae722ee2e8b49d7f2a
羽峰 俎 <3014085426@qq.com>
2022-07-01 18:57:15 +0800

    添加项目文件。

---
+++ Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Net.Http.Handlers;
+
+
+Console.WriteLine("Hello, World!");
+
+long a = 0;
+
+string url = "https://nightly.link/Grasscutters/Grasscutter/workflows/build/development/Grasscutter.zip";
+string filepath = @"D:\Grasscutter.zip";
+DownLoad downLoad = new DownLoad();
+
+await downLoad.DownloadFile(url, filepath, (a) =>
+{
+    Console.WriteLine("已经获得的字节数量："+a);
+});
+
---
+++ DownLoad/DownLoad.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Handlers;
+using System.Threading;
+using System.Threading.Tasks;
+namespace DownLoad
+{
+    public class DownLoad
+    {
+        public int NowProcess = 0;
+        /// <summary>
+        /// 下载文件
+        /// </summary>
+        /// <param name="url">下载host</param>
+        /// <param name="filepath">下载文件的保存地址</param>
+        /// <returns></returns>
+        /// <summary>
+        /// 下载文件
+        /// </summary>
+        /// <param name="url">下载host</param>
+        /// <param name="filepath">下载文件的保存地址</param>
+        /// <returns></returns>
+        public async Task DownloadFile(string url, string filepath, Action<long> action)
+        {
+            var progressMessageHandler = new ProgressMessageHandler(new HttpClientHandler());
+            progressMessageHandler.HttpReceiveProgress += (j, m) =>
+            {
+                var b = m.BytesTransferred;
+                action.Invoke(b);
+            };
+            using (var client = new HttpClient(progressMessageHandler))
+            {
+                using (var filestream = new FileStream(filepath, FileMode.Create))
+                {
+                    var netstream = await client.GetStreamAsync(url);
+                    await netstream.CopyToAsync(filestream);
+                }
+            }
+        }
+
+    }
+
+}
